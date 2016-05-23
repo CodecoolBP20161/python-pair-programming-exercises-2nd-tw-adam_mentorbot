e802783a8c29f3d4c413bb52bcb894dd32d7b523
turbek <turbek93@gmail.com>
2016-05-23 13:32:38 +0200

    implementing listoverlap

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,9 +1,24 @@
+from random import randint
+
+
 def listoverlap(list1, list2):
-    return
+    return list(set(list1).intersection(set(list2)))
+
+
+def create_random_list():
+    to_return = []
+    for i in range(randint(0, 20)):
+        to_return += [randint(0, 20)]
+    return to_return
 
 
 def main():
-    return
+    a = create_random_list()
+    b = create_random_list()
+    result = listoverlap(a, b)
+    print(a)
+    print(b)
+    print(result)
 
 
 if __name__ == '__main__':
