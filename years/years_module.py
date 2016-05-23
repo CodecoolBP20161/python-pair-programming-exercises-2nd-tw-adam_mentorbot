d480bb364c61ee94774522909ce1ab853c3554da
turbek <turbek93@gmail.com>
2016-05-23 13:11:12 +0200

    years module working fine

--- years/years_module.py
+++ years/years_module.py
@@ -1,11 +1,27 @@
 import datetime
+from datetime import date
 
 
 def years(age):
-    return
+    this_year = date.today().year
+    return this_year+100-age
 
 
 def main():
+    name = input("What's your name?")
+    try:
+        age = int(input("What's your age?"))
+    except ValueError:
+        print("Give me a number next time you piece of shit!")
+        exit()
+
+    date = years(age)
+
+    if age >= 100:
+        print("Hey" + name + ", you've already turned 100 in " + str(date))
+    else:
+        print("Hey " + name + ", you'll turn 100 in " + str(date))
+
     return
 
 
