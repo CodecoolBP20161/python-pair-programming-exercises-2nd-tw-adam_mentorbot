efe2af12a138881acdfdf44da369ef6f3dcae8c7
turbek <turbek93@gmail.com>
2016-05-23 14:30:14 +0200

    implementing passwordgen

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,30 @@
+from random import randint
+
+
+abc = ["0123456789",
+       "ABCDEFGHIJKLMNOPQRSTUVXYZ",
+       "abcdefghijklmnopqrstuvxyz",
+       "!@#$%^&*()?"]
+
+
 def passwordgen():
-    return
+    while True:
+        abc_used = [False] * 4
+        password = ""
+        for i in range(8):
+            rand_abc = randint(0, 3)
+            abc_used[rand_abc] = True
+            chars = abc[rand_abc]
+            password += chars[randint(0, len(chars)-1)]
+
+        if abc_used == [True] * 4:
+            break
+
+    return password
 
 
 def main():
+    print(passwordgen())
     return
 
 
